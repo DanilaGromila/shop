c30050e91cf279adb5d54257b10b25f51bfe786d
DanilaGromila <d.gromila@yahoo.com>
2026-03-05 18:34:55 -0800

    revert to empty project

--- src/main/java/org/gromila/shopapp/exception/HibernateException.java
+++
@@ -1,7 +0,0 @@
-package org.gromila.shopapp.exception;
-
-public class HibernateException extends RuntimeException {
-    public HibernateException(String message) {
-        super(message);
-    }
-}
